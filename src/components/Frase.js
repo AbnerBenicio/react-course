75c6e0fc394fcba094aed8c866a04af5365ee5f2
AbnerBenicio <abnerbeniciosilva123@gmail.com>
2023-07-11 20:17:01 -0300

    Fazendo toggle de classes para estilos em module.css

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,10 +1,14 @@
 import styles from "./Frase.module.css"
+import { useState } from "react"
 
 function Frase () {
 
+    let [frase, setFrase] = useState("frase")
+
     return (
         <div>
-            <p className={styles.frase}>Este é um componente!</p>
+            <p className={styles[frase]} onClick={() => {frase.includes("Ativa") ? setFrase("frase"): setFrase("fraseAtiva")} }>Este é um componente!</p>
+
         </div>
     )
 }
