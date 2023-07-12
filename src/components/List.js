7d1a5e5c6842dbbd88f7e2784f1d39fbd1939f6a
AbnerBenicio <abnerbeniciosilva123@gmail.com>
2023-07-12 12:46:06 -0300

    Capturando e exibindo valor do input na tela após apertar enter

--- src/components/List.js
+++ src/components/List.js
@@ -5,15 +5,18 @@ function List() {
   let [val, setVal] = useState();
 
   const teste = (e) => {
-    setVal(e.target.value);
+    if (e.key === "Enter") {
+      setVal(e.target.value);
     if (e.target.value === "") {
       setVal()
     }
+    } 
+
   };
 
   return (
     <>
-      <input onChange={teste} />
+      <input onKeyUp={teste} />
       <h1>Minha Lista</h1>
       <ul>
         <Item marca="toyota" ano_lancamento={1985} />
