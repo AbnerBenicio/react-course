d01bbffa0f0150bf562556695730a62a5bad3743
AbnerBenicio <abnerbeniciosilva123@gmail.com>
2023-07-19 13:18:43 -0300

    Entendendo renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import People from './components/People';
-import Frase from './components/Frase';
-import List from './components/List';
-import Event from './components/Event';
-import Form from './components/Form';
+import RendCondicional from './components/RendCondicional';
+// import SayMyName from './components/SayMyName';
+// import People from './components/People';
+// import Frase from './components/Frase';
+// import List from './components/List';
+// import Event from './components/Event';
+// import Form from './components/Form';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Testando eventos</h1>
-      <Event numero={1}/>
-      <Form />
-      <List />
+      <h1>Renderização condicional</h1>
+      <RendCondicional />
       
     </div>
   );
--- src/components/RendCondicional.js
+++ src/components/RendCondicional.js
@@ -1,5 +1,40 @@
-function RendCondicional () {
-    return ()
+import { useState } from "react";
+
+function RendCondicional() {
+  const [email, setEmail] = useState("");
+  const [useEmail, setuserEmail] = useState("");
+
+  const enviarEmail = (e) => {
+    e.preventDefault();
+    setuserEmail(email);
+  };
+
+  const limparEmail = () => {
+    setuserEmail("")
+  }
+
+  return (
+    <div>
+      <h2>Cadastre o seu e-mail</h2>
+      <form>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Digite seu email"
+        />
+        <button type="submit" onClick={enviarEmail}>
+          Enviar e-mail
+        </button>
+
+        {useEmail && (
+          <div>
+            <p>O email do usuário é {useEmail}</p>
+            <button onClick={limparEmail}>Limpar e-mail</button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
 }
 
-export default RendCondicional
+export default RendCondicional;
